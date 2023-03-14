3769113833860bb2304c8f4d621fd11b8c1d39b0
Student <venus.kabera@student.moringaschool.com>
2023-03-14 21:19:01 +0300

    done

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,6 @@
 class BirdsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
   # GET /birds
   def index
@@ -10,7 +11,8 @@ class BirdsController < ApplicationController
   # POST /birds
   def create
     bird = Bird.create(bird_params)
-    render json: bird, status: :created
+      render json: bird, status: :created
+    
   end
 
   # GET /birds/:id
@@ -23,7 +25,7 @@ class BirdsController < ApplicationController
   def update
     bird = find_bird
     bird.update(bird_params)
-    render json: bird
+    render json: bird  
   end
 
   # DELETE /birds/:id
@@ -45,6 +47,10 @@ class BirdsController < ApplicationController
 
   def render_not_found_response
     render json: { error: "Bird not found" }, status: :not_found
+  end
+
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
   end
 
 end
